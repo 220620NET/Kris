2e9d0495445662793ce16c2c7382dfd62bc48c62
Kristopher Holland <krisholl2022@gmail.com>
2022-06-29 10:01:33 -0400

    Updated
    Signed-off-by: Kristopher Holland <krisholl2022@gmail.com>

--- PKMNStorage/UI/Program.cs
+++ PKMNStorage/UI/Program.cs
@@ -1,4 +1,27 @@
 ﻿using UI;
+using CustomExceptions;
+using System;
 
-MainMenu menu=new MainMenu();
-menu.Start();
+Console.WriteLine("Main method or test case?")
+string please = Console.Readline();
+if(please=="Main"){
+    MainMenu menu=new MainMenu();
+    menu.Start();
+}else{
+    Console.WriteLine("Welcome to Pokemon Storage System!");
+    Console.WriteLine("One convenient place for all your pokemon storage needs!");
+
+    PKMN testPKMN = new PKMN();
+    try
+    {
+        testPKMN.Level = Int32.Parse(Console.ReadLine());
+        // testPokemon.Level = -1;
+    }
+    catch(InputInvalidException ex)
+    {
+        Console.WriteLine("Exception was thrown");
+        Console.WriteLine(ex.Message);
+    }
+
+    Console.WriteLine(testPKMN);
+}
